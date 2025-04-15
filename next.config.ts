e68aad88a37fe6409c4a5342370fc530b7932e64
--global <tilakthapa9898@gmail.com>
2025-04-15 16:10:14 +0545

    fix: update image optimization settings and correct export folder name

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
   images: {
-    unoptimized: true,
+    unoptimized: false,
     minimumCacheTTL: 60 * 99999999,
     qualities: [100],
     loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [48, 96, 128, 384],
+    deviceSizes: [640, 750, 1200, 1920],
     remotePatterns: [
       {
         hostname: "www.svgrepo.com",
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
     nextImageExportOptimizer_exportFolderPath: "out",
     nextImageExportOptimizer_quality: "75",
     nextImageExportOptimizer_storePicturesInWEBP: "true",
-    nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+    nextImageExportOptimizer_exportFolderName: "next-image-export-optimizer",
     nextImageExportOptimizer_generateAndUseBlurImages: "true",
     nextImageExportOptimizer_remoteImageCacheTTL: "0",
   },
